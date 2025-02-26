4d5f3e1ee9e6ecf4c01a7025202d8dcb637d28a3
naddeok96 <naddeok5@students.rowan.edu>
2025-02-26 12:32:06 -0500

    added venv

--- run_autolabel_gui.sh
+++ run_autolabel_gui.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+# Activate the virtual environment
+source /home/naddeok5/envs/auto-label-engine/bin/activate
+
+# Run the Streamlit application in headless mode with file watcher disabled
 streamlit run --server.headless True --server.fileWatcherType none autolabel_gui.py
