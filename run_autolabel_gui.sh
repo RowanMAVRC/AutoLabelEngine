b4237c67382e72771c4d375ddae10be583fb8a13
Kyle <naddeok5@rowan.edu>
2025-03-13 20:54:17 -0400

    setup new venv

--- run_autolabel_gui.sh
+++ run_autolabel_gui.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-source /home/naddeok5/envs/auto-label-engine/bin/activate
+source /home/naddeok5/envs/autolabel//bin/activate
 streamlit run --server.headless True --server.fileWatcherType none autolabel_gui.py
