ed4d76821e5b2678750349074863672a4e04fbe7
Kyle <naddeok5@rowan.edu>
2025-03-30 18:03:19 -0400

    updated to auto make venv

--- run_autolabel_gui.sh
+++ run_autolabel_gui.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-source /home/naddeok5/envs/autolabel//bin/activate
+
+# Check if the virtual environment activation file exists
+if [ ! -f "../envs/auto-label-engine/bin/activate" ]; then
+    echo "Virtual environment not found. Setting up environment..."
+    bash setup_venv.sh ../envs/auto-label-engine
+fi
+
+# Activate the virtual environment
+source ../envs/auto-label-engine/bin/activate
+
+# Run the Streamlit application in headless mode
 streamlit run --server.headless True --server.fileWatcherType none autolabel_gui.py
