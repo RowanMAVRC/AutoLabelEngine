49701881ffc5d30e017704326bf402103da22977
Kyle Naddeo <naddeok5@students.rowan.edu>
2025-05-07 10:26:25 -0400

    upgraded ultralytics so now required model.pt rather than .yaml

--- scripts/train_yolo.py
+++ scripts/train_yolo.py
@@ -3,9 +3,8 @@ from wandb.integration.ultralytics import add_wandb_callback
 from ultralytics import YOLO
 
 def main(model_path, data_path, train_path):
-    print(model_path, data_path, train_path)
     # Load a model from the given model configuration file
-    model = YOLO(model_path)
+    model = YOLO(model_path, task="detect") 
     add_wandb_callback(model, enable_model_checkpointing=True)
     
     model.train(cfg=train_path, data=data_path)
@@ -15,8 +14,8 @@ if __name__ == "__main__":
     parser.add_argument(
         "--model_path",
         type=str,
-        default="cfgs/yolo/model/hololens_combined_v11.yaml",
-        help="Path to the model configuration file"
+        default="yolo12x.pt",
+        help="Path to the model .pt file"
     )
     parser.add_argument(
         "--data_path",
