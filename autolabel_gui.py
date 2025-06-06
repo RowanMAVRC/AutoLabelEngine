185343514de91f333328c39245b8cd9c6f15f448
Kyle Naddeo <naddeok5@students.rowan.edu>
2025-06-06 13:26:13 -0400

    fixed jump page button

--- autolabel_gui.py
+++ autolabel_gui.py
@@ -1334,6 +1334,7 @@ def jump_frame_frame_by_frame_callback():
     if st.session_state.frame_index != new_frame_index:
         st.session_state.frame_index = new_frame_index
         st.session_state["skip_label_update"] = True
+        save_session_state(st.session_state.paths['session_state_path'])
 
 def jump_frame_object_by_object_callback():
     # Get the desired frame number from the number input.
